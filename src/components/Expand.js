53e37d3816c45fc15e9fc0a5fd0ce6140cc8a4e7
ununique3 <j.horst77@gmail.com>
2022-08-30 15:55:01 -0600

    Up to date

---
+++ src/components/Expand.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import {useState} from 'react'
+
+
+const Expand = (props) => {
+const [expand, setExpand] = useState(false)
+  //delete icon ceme from icons8.com
+  //-----------------------------------------------
+  //        EXPAND FUNCTION
+  //-----------------------------------------------
+  const handleExpand = () => {
+    setExpand(!expand)
+  }
+
+  return (
+    <>
+    {expand ?   <div className='card expand'>
+        <img onClick={handleExpand} src={props.movie.image} />
+          <div className='cardOpt'>
+            <img onClick={(event) => {props.handleDelete(props.movie)}} src='icons8-remove-24.png'/>
+          </div>
+        </div> : <div className='card'>
+        <img onClick={handleExpand} src={props.movie.image} />
+          <div className='cardOpt'>
+            <img onClick={(event) => {props.handleDelete(props.movie)}} src='icons8-remove-24.png'/>
+          </div>
+        </div>}
+
+    </>
+  )
+
+
+
+
+
+
+
+
+
+}
+
+
+export default Expand
