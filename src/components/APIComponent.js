5969fc7b439bb0d316d8fdf55bfb08317ca60d5f
Tyler Nenninger <tylernenninger@Annas-Air.lan>
2022-08-30 14:19:23 -0500

    searching

--- src/components/APIComponent.js
+++ src/components/APIComponent.js
@@ -9,13 +9,15 @@ const APIComponent = (props) => {
   const [newRating, setNewRating] = useState(0)
   let [page, setPage] = useState(1)
   let [newQuery, setNewQuery] = useState('')
+  let [selectionOptions, setSelectionOptions] = useState([])
+  let [genreID, setGenreID] = useState(6)
+  let [movieListByGenre, setMovieListByGenre] = useState([])
 
   useEffect(()=>{
       axios
-          .get('https://api.themoviedb.org/3/trending/all/day?api_key=' + apiKey + '&page=' + page)
+          .get('https://api.themoviedb.org/3/trending/all/day?api_key=' + apiKey)
           .then((response)=>{
             console.log(response.data, 'useeffect');
-
               setPopularMovies(response.data.results)
           })
   }, [])
@@ -35,7 +37,7 @@ const APIComponent = (props) => {
       axios
         .get('https://fast-bayou-47205.herokuapp.com/movies')
         .then((response) => {
-          console.log(response.data)
+          //console.log(response.data)
           props.setMovies(response.data)
         })
     })
@@ -46,9 +48,8 @@ const APIComponent = (props) => {
     axios
       .get('https://api.themoviedb.org/3/trending/all/day?api_key=' + apiKey + '&page=' + page)
       .then((response)=>{
-        console.log(response.data);
-
-          setPopularMovies(response.data.results)
+        //console.log(response.data);
+        setPopularMovies(response.data.results)
       })
   }
 
@@ -78,15 +79,49 @@ const APIComponent = (props) => {
       })
   }
 
+  //loading genres selectors from API
+  useEffect(()=>{
+      axios
+          .get('https://api.themoviedb.org/3/genre/movie/list?api_key=' + apiKey + '&language=en-US')
+          .then((response)=>{
+              setSelectionOptions(response.data.genres)
+          })
+  }, [])
+
+  const handleChangeGenre = (e) => {
+    console.log(e.target.value, 'correct')
+    setGenreID(genreID = e.target.value)
+    console.log(genreID, 'doesnt switch')
+
+    axios
+      .get('https://api.themoviedb.org/3/discover/movie?api_key=' + apiKey + '&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_genres=' + genreID)
+      .then((response)=>{
+        setPopularMovies(response.data.results)
+      })
+      //console.log(popularMovies, 'check');
+  }
+
 
 
   return (
     <div>
       <h1>Popular Movies</h1>
+      <select onChange={handleChangeGenre}>
+        <option value='popular'>popular</option>
+        {selectionOptions.map((option) => {
+          return (
+            <option
+              key={option._id}
+              value={option.id}>{option.name}
+            </option>
+          )
+        })}
+      </select><br/><br/>
       <button onClick={handleLoadPreviousPage}>Previous</button>
-      <button onClick={handleLoadNextPage}>Next</button>
+      <button onClick={handleLoadNextPage}>Next</button><br/><br/>
       <input type='text' onKeyUp={handleQueryChange}/>
-      <button onClick={handleQueryFormSubmit}>Search Button</button>
+      <button onClick={handleQueryFormSubmit}>Search by Title</button>
+
       <ul>
         {popularMovies.map((movie) => {
           return (
